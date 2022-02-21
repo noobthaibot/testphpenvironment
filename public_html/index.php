e1695cefcfbb728c1428a8f912fef0db9490c30e
Группа #1 <dgugik@ukr.net>
2022-02-21 19:34:44 +0200

    php ifesle test

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,6 @@
+<?php $isAdmin
 
+?>
 
 
 <!doctype html>
@@ -29,16 +31,19 @@
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+         <?php if ($isAdmin === true): ?>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+         <?php else: ?>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li>
+        <?php endif ?>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
